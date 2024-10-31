16a9306e7a4fe721a2629eaff2aea1313b9ac36e
krizz <154887919+krsna081@users.noreply.github.com>
2024-10-31 22:39:53 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,47 +8,44 @@ function App() {
       <Helmet>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width" />
-        <meta property="og:title" content="Dikira Kira Saja" />
+        <meta property="og:title" content="Community Krizz" />
         <meta property="og:image"
-          content="https://f.balxzzy.xyz/component/IMG-20240928-WA0193.jpg"/>
+          content="https://files.catbox.moe/09wbxp.jpg"/>
         <meta property="og:description"
           content="WhatsApp bot community, WhatsApp bot dev, Community WhatsApp bot, WhatsApp bot drv, wabot"/>
         <meta name="twitter:image:src"
-          content="https://f.balxzzy.xyz/component/IMG-20240928-WA0193.jpg"/>
-        <title>Community</title>
+          content="https://files.catbox.moe/09wbxp.jpg"/>
+        <title>Community Wangsaf</title>
       </Helmet>
       
       <div className="container">
         <img alt="profile picture" className="profile-pic"
-          src="https://f.balxzzy.xyz/component/IMG-20240928-WA0193.jpg"/>
+          src="https://files.catbox.moe/09wbxp.jpg"/>
         <div className="title">
-          Xshiina - Community <i className="fas fa-check-circle"></i>
+          Krizz - Community <i className="fas fa-check-circle"></i>
         </div>
         
-        <div className="subtitle">Beginner Team, Suka Bikin Honar</div>
+        <div className="subtitle">Beginner Team, Hwok Resingg Team</div>
         <div className="social-icons">
-          <a href="https://instagram.com/@iqstore78">
+          <a href="https://instagram.com/@krsna_081">
             <i className="fab fa-instagram"></i>
           </a>
           
-          <a href="https://wa.me/6281276400345?text=hi+balzz">
+          <a href="https://wa.me/6281235807940?text=hi+krizz">
             <i className="far fa-comment-lines"></i>
           </a>
         </div>
 
         <Section title="Social Dev Link">
-          <LinkItem href="https://me.balxzzy.xyz" icon="fa-brands fa-edge" text="My Portfolio"/>
-          <LinkItem href="https://api.balxzzy.xyz" icon="fa-regular fa-fire" text="Projects Rest API's"/>
-          <LinkItem href="https://github.com/balxz" icon="fa-brands fa-github" text="Github"/>
-          <LinkItem href="https://tiktok.com/@iqstore78" icon="fa-brands fa-tiktok" text="Tiktok"/>
+          <LinkItem href="https://www.youtube.com/@krsna_081" icon="fa-brands fa-youtube" text="Youtube"/>
+          <LinkItem href="https://github.com/krsna081" icon="fa-brands fa-github" text="Github"/>
+          <LinkItem href="https://tiktok.com/@krnsa_081" icon="fa-brands fa-tiktok" text="Tiktok"/>
         </Section>
 
-        <Section title="Community - Codings">
-          <LinkItem href="https://chat.whatsapp.com/FW78A12iQIdK3snmkSKzQ3" icon="fab fa-whatsapp" text="Shiina Wabot Groups"/>
-          <LinkItem href="https://whatsapp.com/channel/0029VaSY7Lp8F2pCmQLKNn0g" icon="fab fa-whatsapp" text="Shiina Channels Community"/>
+        <Section title="Community - Group's">
+          <LinkItem href="https://chat.whatsapp.com/Kvc5adketWeLBtaXdsHsvo" icon="fab fa-whatsapp" text="Kurai Wabot Groups"/>
+          <LinkItem href="https://whatsapp.com/channel/0029VaOQ0f3BA1f7HHV9DV1J" icon="fab fa-whatsapp" text="Krizz Channels Community"/>
           <LinkItem href="https://hello.com" icon="fab fa-whatsapp" text="Gmn Team"/>
-          <LinkItem href="https://chat.whatsapp.com/JbzMsezhCwUKdC6dnjwcIz" icon="fab fa-whatsapp" text="All Bot Community"/>
-          <LinkItem href="https://chat.whatsapp.com/ExSzRGszuh2FIiMctAQJMZ" icon="fab fa-whatsapp" text="BinaryCrafters"/>
         </Section>
       </div>
     </div>
